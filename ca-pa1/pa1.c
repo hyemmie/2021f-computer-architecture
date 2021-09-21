62deb0a62877ab8052fe5c9b5d4b979c94ce80e0
hyemmie <lhm0710@naver.com>
2021-09-22 06:15:05 +0900

    refactor encoding

--- ca-pa1/pa1.c
+++ ca-pa1/pa1.c
@@ -20,8 +20,7 @@
 int encode(const char* const src, const int srclen, char* const dst, const int dstlen)
 {
 
-    int zero_count = 0;
-    int one_count = 0;
+    int run_length = 0;
     int find_case = 0;
     int current_dst_length = 0;
     int current_dst_bit = 0;
@@ -31,93 +30,37 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
     for (int i = 0; i < srclen; i++) {
         for (int j = 7; j >= 0; j--) {
             int bit = (src[i] >> j) & 1;
-            // counting zero
-            if (find_case == 0) {
-                // encounter zero
-                if (bit == 0) {
-                    // add 7th zero to 6 zeros (run length == 7)
-                    if (zero_count >= 6) {
-                        zero_count++;
-                        // write number of zero to memory
-                        for (int i = 2; i >= 0; i--) {
-                            int temp = (zero_count >> i) & 1;
-                            if (current_dst_bit >= 7) {
-                                if (current_dst_length >= dstlen) {
-                                    return -1;
-                                }      
-                                *(dst+current_dst_length++) = (temp_dst_bit << 1) | temp;
-                                temp_dst_bit = 0;
-                                current_dst_bit = 0;
-                            } else {
-                                current_dst_bit++;
-                                temp_dst_bit = (temp_dst_bit << 1) | temp;
-                            }
-                        }
-                        zero_count = 0;
-                        find_case = 1;
-                    }
-                    // run length < 7
-                    else { 
-                        zero_count++; 
-                    }
-                }
-                // encounter one
-                else {
-                    // write number of zero to memory
+            // counting current case
+            if (find_case == bit) {
+                // run length == 7
+                if (run_length >= 6) {
+                    run_length++;
                     for (int i = 2; i >= 0; i--) {
-                        int temp = (zero_count >> i) & 1;
-                            if (current_dst_bit >= 7) {
-                                current_dst_bit = 0;
-                                if (current_dst_length >= dstlen) {
-                                    return -1;
-                                } 
-                                *(dst+current_dst_length++) = (temp_dst_bit << 1) | temp;
-                                temp_dst_bit = 0;
-                            } else {
-                                current_dst_bit++;
-                                temp_dst_bit = (temp_dst_bit << 1) | temp;
-                            }
+                        int temp = (run_length >> i) & 1;
+                        if (current_dst_bit >= 7) {
+                            if (current_dst_length >= dstlen) {
+                                return -1;
+                            }      
+                            *(dst+current_dst_length++) = (temp_dst_bit << 1) | temp;
+                            temp_dst_bit = 0;
+                            current_dst_bit = 0;
+                        } else {
+                            current_dst_bit++;
+                            temp_dst_bit = (temp_dst_bit << 1) | temp;
+                        }
                     }
-                    zero_count = 0;
-                    find_case = 1;
-                    one_count++;
+                    run_length = 0;
+                    find_case = find_case == 1 ? 0 : 1;
+                }
+                // run length < 7
+                else { 
+                    run_length++; 
                 }
             }
-            // counting one
+            // encounter diff -> write current number and switch case
             else {
-                // encounter one
-                if (bit == 1) {
-                    // add 7th one to 6 one(run length == 7)
-                    if (one_count >= 6) {
-                        one_count++;
-                        // write number of one to memory
-                        for (int i = 2; i >= 0; i--) {
-                            int temp = (one_count >> i) & 1;
-                            if (current_dst_bit >= 7) {
-                                current_dst_bit = 0;
-                                if (current_dst_length >= dstlen) {
-                                    return -1;
-                                } 
-                                *(dst+current_dst_length++) = (temp_dst_bit << 1) | temp;
-                                temp_dst_bit = 0;
-                            } else {
-                                current_dst_bit++;
-                                temp_dst_bit = (temp_dst_bit << 1) | temp;
-                            }
-                        }
-                        one_count = 0;
-                        find_case = 0;
-                    }
-                    // run length < 7
-                    else { 
-                        one_count++;
-                    }
-                }
-                // encounter zero
-                else {
-                    // write number of one to memory
-                    for (int i = 2; i >= 0; i--) {
-                        int temp = (one_count >> i) & 1;
+                for (int i = 2; i >= 0; i--) {
+                    int temp = (run_length >> i) & 1;
                         if (current_dst_bit >= 7) {
                             current_dst_bit = 0;
                             if (current_dst_length >= dstlen) {
@@ -129,19 +72,17 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
                             current_dst_bit++;
                             temp_dst_bit = (temp_dst_bit << 1) | temp;
                         }
-                    }
-                    one_count = 0;
-                    find_case = 0;
-                    zero_count++;
                 }
+                run_length = 1;
+                find_case = find_case == 1 ? 0 : 1;
             }
         }
     }
-    // add remaining counted zero
-    if (find_case == 0) {
+    // add remaining number
+    if (run_length > 0) {
         // write number of zero to memory
         for (int i = 2; i >= 0; i--) {
-            int temp = (zero_count >> i) & 1;
+            int temp = (run_length >> i) & 1;
             if (current_dst_bit >= 7) {
                 current_dst_bit = 0;
                 if (current_dst_length >= dstlen) {
@@ -161,31 +102,6 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
             } 
             *(dst+current_dst_length++) = temp_dst_bit << (8 - current_dst_bit);
         }
-    }
-    // add remaining counted one
-    else {
-        // write number of one to memory
-        for (int i = 2; i >= 0; i--) {
-            int temp = (one_count >> i) & 1;
-            if (current_dst_bit >= 7) {
-                current_dst_bit = 0;
-                if (current_dst_length >= dstlen) {
-                    return -1;
-                } 
-                *(dst+current_dst_length++) = (temp_dst_bit << 1) | temp;
-                temp_dst_bit = 0;
-            } else {
-                current_dst_bit++;
-                temp_dst_bit = (temp_dst_bit << 1) | temp;
-            }
-        }
-        // add padding
-        if (current_dst_bit > 0) {
-            if (current_dst_length >= dstlen) {
-                return -1;
-            } 
-            *(dst+current_dst_length++) = temp_dst_bit << (8 - current_dst_bit);
-        }    
     }
 
     // output is bigger than dstlen
