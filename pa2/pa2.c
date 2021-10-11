d012da86409befddfecf1fe278b4c743ed8f8dbd
hyemmie <lhm0710@naver.com>
2021-10-11 23:57:46 +0900

    feat int to fp10

--- pa2/pa2.c
+++ pa2/pa2.c
@@ -20,16 +20,37 @@
 /* Convert 32-bit signed integer to 10-bit floating point */
 fp10 int_fp10(int n)
 {
-	/* TODO */
-
-
-
-
-
-
-
-
-	return 1;
+	fp10 ans = 0;
+	if (n == 0) return ans; // 0
+	// if () // NaN
+	if (n >= 0xFC00) return (ans + (31 << 4)); // Inf
+	// negative sign
+	if (n < 0) {
+		ans += (0xFFFFFFFF << 9);
+		n *= (-1);
+	}
+	int exp = 0;
+	int temp_n = n;
+	while (temp_n > 1) {
+		temp_n >>= 1;
+		if (++exp > 15) return (ans + (31 << 4));
+	}
+	int frac = (exp < 5) ? ((n - (1 << exp)) << (4 - exp)) : (((n - (1 << exp)) >> (exp - 4)));
+	// calculate sticky bit
+	int s = n & ((1 << (exp - 5)) - 1);
+	int r = n & (1 << (exp - 5));
+	int l = n & (1 << (exp - 4));
+	// rounding
+	if (r > 0) {
+		if (s > 0) frac += 1;
+		if (s == 0 && l > 0) frac += 1;
+	}
+	if (frac > 15) {
+		if (++exp > 15) return (ans + (31 << 4));
+		frac = 0;
+	} 
+	
+	return ans + ((exp + 15) << 4) + frac;
 }
 
 /* Convert 10-bit floating point to 32-bit signed integer */
