bc16db166718ebc98d81ef8813da2b85f5f43e3f
hyemmie <lhm0710@naver.com>
2021-11-12 06:22:05 +0900

    feat 9 input

--- pa3/bmpconv.s
+++ pa3/bmpconv.s
@@ -20,6 +20,7 @@
 ####################
 
 # x0, sp, ra, a0 ~ a4, t0 ~ t4
+# outputptr 초기화하기
 
 	.globl bmpconv
 bmpconv:
@@ -32,7 +33,7 @@ bmpconv:
 	beq x0, x0, height_loop
 
 height_loop: 
-	ble t0, a1, end_height
+	bge t0, a1, end_height
 	# t1 = h-1
 	addi t1, a1, -1
 	beq t0, t1, height_1
@@ -41,14 +42,14 @@ height_loop:
 	beq t0, t1, height_2
 
 	# t1이 일반적인 경우
-	ble t0, t1, height_3
+	blt t0, t1, height_3
 
 	height_1:
 		# t2 = c (1)
 		addi sp, sp, -4
 		addi t2, x0, 1
 		sw t2, 0(sp)
-		ret
+		beq x0, x0, prepare_width
 		# t2 써도 됨
 
 	height_2: 
@@ -56,7 +57,7 @@ height_loop:
 		addi sp, sp, -4
 		addi t2, x0, 2
 		sw t2, 0(sp)
-		ret
+		beq x0, x0, prepare_width
 		# t2 써도 됨
 
 	height_3: 
@@ -64,20 +65,20 @@ height_loop:
 		addi sp, sp, -4
 		addi t2, x0, 3
 		sw t2, 0(sp)
-		ret
+		beq x0, x0, prepare_width
 		# t2 써도 됨
+	
+	prepare_width: # t1 써도됨 그래서 t1 = i로
+		addi t1, x0, 0
+		# t2 = 3(w+1) / 4
+		addi t2, a2, 1
+		slli t3, t2, 1
+		add t2, t3, t2
+		srli t2, t2, 2
+		# t2, t3 써도됨
+		beq x0, x0, width_loop
 
-	# t1 써도됨 그래서 t1 = i로
-	addi t1, x0, 0
-	# t2 = 3(w+1) / 4
-	addi t2, a2, 1
-	slli t3, t2, 1
-	add t2, t3, t2
-	srli t2, t2, 2
-	# t2, t3 써도됨
-	ble t1, t2, width_loop
-
-# t2 바꿀거임 
+# t2 바꿀거임
 	width_loop:
 		bge t1, t2, end_width
 		# t2 = 3(w+1) / 4 - 1
@@ -88,14 +89,14 @@ height_loop:
 		beq t1, t2, width_2
 
 		# t1(i)이 일반적인 경우
-		ble t1, t2, width_3
+		blt t1, t2, width_3
 
 		width_1:
 			# t3 = d (1)
 			addi sp, sp, -4
 			addi t3, x0, 1
 			sw t3, 0(sp)
-			ret
+			beq x0, x0, prepare_outer
 			# t3 써도됨
 
 		width_2: 
@@ -103,7 +104,7 @@ height_loop:
 			addi sp, sp, -4
 			addi t3, x0, 2
 			sw t3, 0(sp)
-			ret
+			beq x0, x0, prepare_outer
 			# t3 써도됨
 
 		width_3: 
@@ -111,30 +112,42 @@ height_loop:
 			addi sp, sp, -4
 			addi t3, x0, 3
 			sw t3, 0(sp)
-			ret
+			beq x0, x0, prepare_outer
 			# t3 써도됨
-
-			# t2 = 3(w+1) / 4 - 1 였는데 바꿀 것! 8의 배수인지 보려고
-		andi t2, t1, 0x007
-		# t2가 0이면, 즉 i가 8의 배수이면 i = i+1하고 width_loop로
-		beq t2, x0, divisible_8
-
+		
+		# 8보다 큰 t1에 대해서 t2가 0이면, 즉 i가 8의 배수이면 i = i+1하고 width_loop로
 		divisible_8: 
+			andi t2, t1, 0x007
+			# 8의 배수가 아니면 넘어감
+			bne t2, x0, prepare2_outer
+			# 8의 배수이면 i = i+1하고 width_loop로 
 			addi t1, t1, 1
+			# width_loop로 돌아가기 전에 t2 = 3(w+1) / 4 계산
+			addi t2, a2, 1
+			slli t3, t2, 1
+			add t2, t3, t2
+			srli t2, t2, 2
+			addi sp, sp, 4
 			beq x0, x0, width_loop
+
+	
+	prepare_outer:
+		# t2 = 3(w+1) / 4 - 2 였는데 바꿀 것! 8의 배수인지 보려고
+		andi t2, t1, 0x7f8
+		bltu x0, t2, divisible_8
 
 		# 지금 쓸 수 있는 레지스터 t2, t3, t4, a3, a4, ra
 		# 근데 여기 for문 두 개 돌리기 전에 메모리에서 c, d 뽑아야 함
+	prepare2_outer:
 		# a3 = d
 		lw a3, 0(sp)
 		# a4 = c
 		lw a4, 4(sp)
-
-
 		# t2 = j = m
 		addi t2, t0, 0
 		# a4 = c + m
-		add a4, t0, t2
+		add a4, a4, t2
+		beq x0, x0, outer
 
 	outer: 
 		# j >= m+c면 out
@@ -146,10 +159,10 @@ height_loop:
 		addi t3, t1, 0
 		# a3 = i + d
 		add a3, a3, t1
+		beq x0, x0, inner
 
 	inner: 
 		bge t3, a3, end_inner
-		
 		# t4 = (w+1)*3
 		addi t4, a2, 1
 		slli ra, t4, 1
@@ -157,38 +170,51 @@ height_loop:
 		# m과 곱해줌
 
 		addi ra, x0, 0
+		addi a4, x0, 0
 		beq ra, x0, mul_m
 
-		# a4에 m*3(w+1) 저장
+		# a4에 m*3(w+1) /4 저장
 		mul_m: 
-			bge ra, t2, fin_mul
+			bge t0, ra, fin_mul
 			add a4, a4, t4
+			srli a4, a4, 2 
 			addi ra, ra, 1
 			beq x0, x0, mul_m
 
 	#t4 사용가능
 
+	# temp:
+	# 		ebreak
+
 	fin_mul:
-		slli t4, t1, 2
+		slli t4, t3, 2
+		slli a4, a4, 2
 		add t4, t4, a4
 		# a4 사용가능
 		add t4, t4, a0
-		lw a4, 0(t4)
-		addi sp, sp, -4
-		sw a4, 0(sp)
-		ebreak
+
+		# lw a4, 0(t4)
+
+		# addi sp, sp, -4
+		# sw a4, 0(sp)
+		
+		# 여기서 outputptr과 k에 접근할 방법이 없음
+		
+		# addi ra, x0, 0
+		# beq t4, ra, temp
+
 		addi t3, t3, 1
 		beq x0, x0, inner
 
 	end_inner:
 		addi t2, t2, 1
 		# a4 스택에 저장했던 것 다시 a4에 돌려놓고 스택에서 빼기
-		sw a4, 0(sp)
+		lw a4, 0(sp)
 		addi sp, sp, 4
 		beq x0, x0, outer
 
 	end_outer:
-
+		
 		beq x0, x0, kernal
 
 	kernal:
@@ -203,18 +229,25 @@ height_loop:
 		slli t3, t2, 1
 		add t2, t3, t2
 		srli t2, t2, 2
+		# i = i+2
+		addi t1, t1, 2
 		beq x0, x0, width_loop
 
 	
-	end_width: # c 꺼내기 다음 m에 의해서 결정됨
+	end_width: # 현재 c 꺼내기 다음 c는 다음 m에 의해서 결정됨
 		addi sp, sp, 4
-		# m < h 면 돌아가기 
-		ble t0, a1, height_loop
+		# m = m + 3
+		addi t0, t0, 3
+		beq x0, x0, height_loop
 
 
 end_height:
 
-	ret
-
-
-	ret
+	# k랑 outputptr 꺼내기 근데 여기가 아니라 kernal에서 해야함 사실
+	# addi sp, sp, 8 
+	
+	addi a0, x0, 0
+	lui a0, 0x80000
+	addi a0, a0, 0x008
+	jalr ra, a0
+ret
